abb5e2c26df13af0d8529201eb258f39ea3c8943
PandaFlo <Flashmait@hotmail.com>
2024-07-24 13:05:16 -0400

    components

--- hotelapp-angular/src/app/app.module.ts
+++ hotelapp-angular/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { HomeDisplayComponent } from './components/home-display/home-display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    HomeDisplayComponent
   ],
   imports: [
     BrowserModule,
